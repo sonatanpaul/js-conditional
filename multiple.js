05507d3be2c5c36abc380bfe41902d9407952cb1
sonatanpaul <iamsonatanpaul@gmail.com>
2024-11-18 20:44:29 +0600

    logical && and logical || practice task is complete

---
+++ multiple.js
@@ -0,0 +1,20 @@
+let mangoPrice = 250;
+let isMangoGood = true;
+
+// if (mangoPrice > 240) {
+//   console.log("I will buy Mango");
+// } else {
+//   console.log("I will cant buy Mango");
+// }
+
+// if (mangoPrice < 300 || isMangoGood == true) {
+//   console.log("i will buy mango");
+// } else {
+//   console.log("i can't buy Mango");
+// }
+
+if (mangoPrice <= 250 && isMangoGood == true) {
+  console.log("I will buy 4 kg Mango");
+} else {
+  console.log("I will buy Mango 1 Kg");
+}
