40a1f5d8328b843dccb40c0a0f63f325f674b28f
sonatanpaul <iamsonatanpaul@gmail.com>
2024-11-18 20:13:01 +0600

    if else practice task complete

---
+++ if-else.js
@@ -0,0 +1,24 @@
+// with out else conditional practice
+
+if (5 > 10) {
+  console.log("Hello world");
+}
+// ans: false ,, code not execute
+
+if (10 > 8) {
+  console.log("Hello world");
+}
+
+// with else conditional
+
+if (12 < 5) {
+  console.log("Now my turn");
+} else {
+  console.log("Not My Turn");
+}
+
+if (5 < 10) {
+  console.log("Now my turn");
+} else {
+  console.log("Not my turn");
+}
--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-console.log(5 > 10); // false
-console.log(10 > 5); // true
-console.log(5 < 8); // true
-console.log(15 < 5); // false
-console.log(14 >= 14); // true
-console.log(15 >= 12); //true
-console.log(12 >= 14); //false
-console.log(4 <= 4); //true
-console.log(8 <= 14); //true
-console.log(14 <= 8); //false
-
-console.log(10 == 10); //true
-console.log(100 == "100"); //true
-console.log(100 === "100"); // false
-console.log(100 !== "100"); // true
-console.log("100" != 100); // false
+console.log("Hello World");
